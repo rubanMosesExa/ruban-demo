c098919e757176992c823344f7c6c2723bc97b69
Ruban Moses <ruban.moses@exathought.com>
2021-12-10 17:56:02 +0530

    first commit messsage

---
+++ src/main/java/com/exathought/app/rubandemo/RubanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.exathought.app.rubandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubanDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exathought/app/rubandemo/RubanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exathought.app.rubandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
